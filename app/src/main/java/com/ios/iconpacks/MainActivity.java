22f388251e621ac81ce6cc1dd9011365d8c152f7
jonjeexe <jonje.exe@gmail.com>
2026-07-04 11:26:39 +0530

    defind layout

--- app/src/main/java/com/ios/iconpacks/MainActivity.java
+++ app/src/main/java/com/ios/iconpacks/MainActivity.java
@@ -1,16 +1,12 @@
-package com.ipack.exe;
+package com.ios.iconpacks;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView tv = new TextView(this);
-        tv.setText("Icon pack installed. Select it in your launcher's Icon Pack settings.");
-        tv.setPadding(40, 100, 40, 40);
-        setContentView(tv);
+        setContentView(R.layout.activity_main);
     }
 }
